251822e383fc2033658cd4babe8c6c4fd83acb27
Nneji Obioma <nnejiobioma@gmail.com>
2023-02-12 20:33:19 +0100

    added file to main.c

--- My_Shell_Project/main.c
+++ My_Shell_Project/main.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
-int main(int ac, char **argv){
+int main(int ac, char **argv)
+{
+	char *prompt = "(reactJav1) $ "; // Creates the initial prompt
+	char *lineptr;					 // store the address of the buffer holding whatever was typed.
+	size_t n = 0;					 // to store the allocated size in bytes
 
+	/* declaring void varriables*/
+	(void)ac;
+	(void)argv;
+
+	printf("%s", prompt);		  // prins out prompt created
+	getline(&lineptr, &n, stdin); // Allocates memory dynamicaly
+	printf("%s\n", lineptr);	  // printing out what was typed
+
+	free(lineptr); // free up assigned memory
 	return (0);
 }
